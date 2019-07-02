a232683c240179b6919ef22aeb94cd7466b06657
Gen TAKAHASHI <kuramechan428@gmail.com>
2019-07-02 23:02:12 +0900

    スクリプトファイルを読み込む関数を追加

--- Isis/Isis/Program.cs
+++ Isis/Isis/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,26 @@ namespace Isis
 	{
 		static void Main(string[] args)
 		{
+			var script = ReadScript();
+
+			foreach (var line in script)
+			{
+				Console.WriteLine(line);
+			}
+		}
+
+		static IEnumerable<string> ReadScript()
+		{
+			string fileName = "script.txt";
+
+			using (var fs = new FileStream(fileName, FileMode.Open, FileAccess.Read))
+			using (var sr = new StreamReader(fs))
+			{
+				while (!sr.EndOfStream)
+				{
+					yield return sr.ReadLine();
+				}
+			}
 		}
 	}
 }
