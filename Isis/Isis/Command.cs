155a1142c23cca7c6f80d86dc35a978463a5e6df
Gen TAKAHASHI <kuramechan428@gmail.com>
2019-07-04 00:09:39 +0900

    シナリオ内のコマンドを扱うクラスの追加

---
+++ Isis/Isis/Command.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Isis
+{
+	/// <summary>
+	/// シナリオコマンドを表します。
+	/// </summary>
+	class Command
+	{
+		/// <summary>
+		/// シナリオコマンドの名前です。
+		/// </summary>
+		public string Name { get; }
+
+		/// <summary>
+		/// シナリオコマンドを入力するための正規パターンです。
+		/// </summary>
+		public Regex InputPattern { get; }
+
+		/// <summary>
+		/// シナリオを出力する位置を表す正規パターンです。
+		/// </summary>
+		public Regex OutputPattern { get; }
+
+		public Command(string name, string inputPattern, string outputPattern)
+		{
+			Name = name;
+			InputPattern = new Regex(inputPattern, RegexOptions.Compiled);
+			OutputPattern = new Regex(outputPattern, RegexOptions.Compiled);
+		}
+	}
+}
